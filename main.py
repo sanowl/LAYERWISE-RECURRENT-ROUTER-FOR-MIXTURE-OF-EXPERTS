40443569883621169d85744535f9b4814f9f0cdf
San <sanowl98@gmail.com>
2024-10-16 20:34:34 +0300

    still  not sure of it so idk yet

--- main.py
+++ main.py
@@ -26,3 +26,24 @@ class RecurrentMoE(nn.Module):
                 ) for _ in range(num_experts)
             ]) for _ in range(num_layers)
         ])
+
+    def forward(self, x):
+        batch_size, seq_len, _ = x.size()
+        h = torch.zeros(1, batch_size * seq_len, self.hidden_size).to(x.device)
+        
+        for layer in range(self.num_layers):
+            x_proj = self.projectors[layer](x).view(batch_size * seq_len, 1, -1)
+            _, h = self.gru(x_proj, h)
+            router_output = self.routers[layer](h.squeeze(0))
+            probs = F.softmax(router_output, dim=-1)
+            top_k_probs, top_k_indices = torch.topk(probs, self.k, dim=-1)
+            top_k_probs = top_k_probs / top_k_probs.sum(dim=-1, keepdim=True)
+            expert_outputs = torch.zeros_like(x)
+            for i in range(self.k):
+                expert_idx = top_k_indices[:, i]
+                expert_prob = top_k_probs[:, i].unsqueeze(-1)
+                expert_output = torch.stack([self.experts[layer][idx.item()](x[b]) for b, idx in enumerate(expert_idx)])
+                expert_outputs += expert_prob * expert_output
+            x = x + expert_outputs
+
+        return x
